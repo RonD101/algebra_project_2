2de7163abb759913cd9aca2293122389fbf973d7
RonD101 <ron.dahan@campus.technion.ac.il>
2020-12-11 09:32:14 +0200

    A_P_2

---
+++ cmake-build-debug/CMakeFiles/algebra_project_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algebra_project_2.dir/main.cpp.o"
+  "algebra_project_2"
+  "algebra_project_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algebra_project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
