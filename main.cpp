bfed42bf6d6ae078a8fddcca478eac53acf2d3c8
malul88 <malul88>
2020-12-11 09:45:52 +0200

    m8

--- main.cpp
+++ main.cpp
@@ -1,27 +1,126 @@
 #include <iostream>
-#include <vector>
-using std::vector;
 
-bool inColumn(int col, int num, int N);
-bool inRow(int row, int num, int N);
+using namespace std;
+
+void boards_counter(int N, int &counter, int **board);
+
+bool isSafe(int i, int j, int **board, int N, int num);
+
+void delete_num(int i, int j, int **board);
+
+bool allBoardFilled(int **board, int N, int &i, int &j);
+
+void printBoard(int **board,int N);
+
+
+bool boardIsValid(int **board, int N);
 
 int main() {
-    vector<vector<int>> matrix;
+    const int N = 4;
+    int rows = N, cols = N;
+    int **board = new int *[rows];
+    board[0] = new int[rows * cols];
+    for (int i = 1; i < rows; ++i)
+        board[i] = board[0] + i * cols;
+    for (int j = 0; j < N; ++j) {
+        for (int i = 0; i < N; ++i) {
+            if (i == 0) {
+                board[i][j] = j+1;
+            } else if (j == 0) {
+                board[i][j] = i+1;
+            } else {
+                board[i][j] = 0;
+            }
+        }
+    }
+    int counter = 0;
+    boards_counter(N, counter, board);
+    cout << counter;
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 
-bool inColumn(vector<vector<int>>& matrix, int col, int num, int N) {
-    for (int row = 0; row < N; row++)
-        if (matrix[row][col] == num)
-            return true;
-    return false;
+void boards_counter(int N, int &counter, int **board) {
+    int i, j;
+    if (allBoardFilled(board, N, i, j)) {
+        if (boardIsValid(board,N)) {
+            printBoard(board, N);
+            counter++;
+            return;
+        }
+        else return;
+    }
+
+    for (int num = 1; num <= N; num++) {
+
+        // Check if looks promising
+        if (isSafe(i, j, board, N, num)) {
+
+            // Make tentative assignment
+            board[i][j] = num;
+
+            // Return, if success
+            boards_counter(N, counter, board);
+
+            // Failure, unmake & try again
+            delete_num(i, j, board);
+        }
+    }
+
+    // This triggers backtracking
 }
 
-bool inRow(vector<vector<int>>& matrix, int row, int num, int N) {
-    for (int col = 0; col < N; col++)
-        if (matrix[row][col] == num)
-            return true;
-    return false;
+bool boardIsValid(int **board, int N) {
+    for (int i = 0; i < N ; ++i) {
+        for (int j = 0; j < i ; ++j) {
+            if (board[i][j] != board[j][i]){
+                return false;
+            }
+        }
+    }
+    return true;
+
+}
+
+bool isSafe(int i, int j, int **board, int N, int num) {
+    bool is_row = false, is_column = false;
+    for (int k = 0; k < N; ++k) {
+        if (board[i][k] == num) {
+            is_column = true;
+            break;
+        }
+    }
+    for (int l = 0; l < N; ++l) {
+        if (board[l][j] == num) {
+            is_row = true;
+            break;
+        }
+    }
+    return !(is_column || is_row);
+}
+
+void delete_num(int i, int j, int **board) {
+    board[i][j] = 0;
+}
+
+bool allBoardFilled(int **board, int N, int &i, int &j) {
+    for ( i = 1; i < N; ++i) {
+        for ( j = 1; j < N; ++j) {
+            if (board[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void printBoard(int **board,int N){
+    for (int i = 0; i < N ; ++i) {
+        for (int j = 0; j < N ; ++j) {
+            cout << board[i][j];
+            if (j == N-1)
+            cout << endl;
+        }
+    }
+    cout << "_______"<< endl;
 }
